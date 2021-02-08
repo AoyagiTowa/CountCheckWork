c0031f8da5aca3fdc78dbe11eb4fc481da1c0440
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-08 14:36:43 +0900

    Create app

--- CheckWork/ViewController.swift
+++ CheckWork/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plus(){
+        number += 1
+        label.text = String(number)
+    }
+    @IBAction func minus(){
+        number -= 1
+        label.text = String(number)
+    }
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
     }
 
 
